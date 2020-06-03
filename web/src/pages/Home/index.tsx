8cf336c884662e61508d921429a5a5037d6b6c8b
AndreVTavares <andretavares3@gmail.com>
2020-06-03 18:29:08 -0300

    Starting Landing Page and CSS files

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css'
+import logo from '../../assets/logo.svg'
+
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                <header>
+                    <img src={logo} alt="Ecoleta"/>
+                </header>
+
+                <main></main>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
