da08565da9e03eb49fe7a202dd57a7cceef37752
AndreVTavares <andretavares3@gmail.com>
2020-06-03 13:56:50 -0300

    criação do metodo show em PointsController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,14 +4,16 @@ import knex from './database/connection';
 import PointsController from './controller/PointsController';
 import ItemsController from './controller/ItemsController';
 
+
 const routes = express.Router();
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-// HOME ////
-routes.get('/items', itemsController.index);
+// index, show, create, update, destroy ////
 
+routes.get('/items', itemsController.index);
 routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 
 
