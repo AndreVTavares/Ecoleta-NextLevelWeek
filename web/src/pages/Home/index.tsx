663955c96f4c6c03bae3b1b6c625da9ee659ba27
AndreVTavares <andretavares3@gmail.com>
2020-06-04 08:40:27 -0300

    Create Home and CreatePoint Pages and Routers

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
 import './styles.css'
 import logo from '../../assets/logo.svg'
 
@@ -10,7 +13,17 @@ const Home = () => {
                     <img src={logo} alt="Ecoleta"/>
                 </header>
 
-                <main></main>
+                <main>
+                    <h1>Seu marketplace de coleta de resíduos.</h1>
+                    <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+                    <Link to="/create-point">
+                        <span>
+                            <FiLogIn/>
+                        </span>
+                        <strong>Cadastre um ponto de coleta</strong>
+                    </Link>
+                </main>
             </div>
         </div>
     )
