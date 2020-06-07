440027e3fac03caedea663a5f04c31981292e252
AndreVTavares <andretavares3@gmail.com>
2020-06-07 09:26:26 -0300

    Upload de imagens back end

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,15 @@
 import express from 'express';
-import knex from './database/connection';
+
+import multer from 'multer';
+import multerConfig from './config/multer'
 
 import PointsController from './controller/PointsController';
 import ItemsController from './controller/ItemsController';
 
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
@@ -13,7 +17,7 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsController.create);
+routes.post('/points', upload.single('image'), pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
